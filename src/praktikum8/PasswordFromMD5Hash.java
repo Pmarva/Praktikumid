150a1c314d887adf9a5105185ada205184123bfd
Pmarva <marvin002msn.com>
2015-10-24 11:01:57 +0300

    MD5 parandus, pole veel valmis

--- src/praktikum8/PasswordFromMD5Hash.java
+++ src/praktikum8/PasswordFromMD5Hash.java
@@ -1,7 +1,7 @@
 package praktikum8;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import java.security.MessageDigest;
-import org.apache.commons.codec.digest;
 
 /**
  * Created by Marva on 23.10.2015.
@@ -12,20 +12,26 @@ public class PasswordFromMD5Hash {
         //result k4la
         String md5Hash = "fd21ae5b17d8a85257936fc23c78ca5c";
         String test = "k4la";
-    try{
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        md.update(test.getBytes());
-        byte[] diggest = md.digest();
-        StringBuffer sb = new StringBuffer();
-        DigestUtils test Digest
+        outerLoop:
+        for(char a = 'a'; a <= 'z';a++) {
+            for(char b = '1'; b<='9';b++) {
+                for(char c = 'a'; c <= 'z';c++) {
+                    for(char d = 'a'; d <= 'z';d++) {
+                        StringBuffer parool = new StringBuffer();
+                        parool.insert(0,a).insert(1,b).insert(2,c).insert(3,d);
+                        String paroola=parool.toString();
+                        String hash = DigestUtils.md5Hex(paroola);
+                        System.out.println(paroola);
 
-    }catch (Exception e) {
+                        if(hash.equals(md5Hash)) {
+                            System.out.println("Success!!!!");
+                            System.out.println(paroola);
 
-    }
-
-
-
-
-
+                            break outerLoop;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
