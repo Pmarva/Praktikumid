f58e4ec2da93a172e70b8de61095fc3b14c227de
Pmarva <marvin002msn.com>
2015-11-23 18:34:36 +0200

    loputoo

--- src/loputoo/Server.java
+++ src/loputoo/Server.java
@@ -12,64 +12,91 @@ import java.util.ArrayList;
  */
 public class Server {
 
-    private ArrayList<ChatRoom> chatRooms = new ArrayList<ChatRoom>();
+    public static ArrayList<ChatRoom> chatRooms = new ArrayList<ChatRoom>();
+    public static ArrayList<User> clients = new ArrayList<User>();
+
 
     public static void main(String[] args) throws Exception {
-            ServerSocket serverSocket = new ServerSocket(12900, 100, InetAddress.getByName("Localhost"));
-            System.out.println("Server started at:" + serverSocket);
 
-        while(true) {
+        ServerSocket serverSocket = new ServerSocket(12900, 100, InetAddress.getByName("Localhost"));
+        System.out.println("Server started at:" + serverSocket);
+
+        while (true) {
             System.out.println("Waiting for connection ...");
             Socket activeSocket = serverSocket.accept();
             Runnable runnable = () -> clientCommunication(activeSocket);
-            new Thread(runnable).start(); // start a new thread
+            new Thread(runnable).start();
         }
     }
 
-
-    private static void clientCommunication(Socket activeSocket){
+    public static void clientCommunication(Socket activeSocket) {
         try {
             User kasutaja;
             BufferedReader socketReader = new BufferedReader(new InputStreamReader(activeSocket.getInputStream()));
             BufferedWriter socketWriter = new BufferedWriter(new OutputStreamWriter(activeSocket.getOutputStream()));
-
             socketWriter.write("Sisestage kasutajanimi:");
             socketWriter.newLine();
             socketWriter.flush();
 
-            String nimi=socketReader.readLine();
-            kasutaja = new User(nimi);
-            socketWriter.write("Tere: "+kasutaja.toString());
+            String nimi = socketReader.readLine();
+            kasutaja = new User(nimi,socketWriter);
+            clients.add(kasutaja);
+            socketWriter.write("Tere: " + kasutaja.toString());
             socketWriter.newLine();
             socketWriter.flush();
-            //System.out.println("Yhendus " + activeSocket);
-            //socketWriter.write("Tere, siia võite midagi kirjutada...");
-            //socketWriter.newLine();
-            //socketWriter.flush();
-
             String teade = null;
 
-            while((teade = socketReader.readLine()) != null) {
-                if (teade.equals("exit")) {
-                    break;
-                }
-                //socketWriter.write(teade);
-                //socketWriter.newLine();
-                //socketWriter.flush();
-                System.out.println("Message from "+kasutaja+": "+teade);
-            }
+            while ((teade = socketReader.readLine()) != null) {
 
-            socketWriter.close();
+                if (teade.equals("exit")) {
+                    clients.remove(kasutaja);
+                    break;
+                } else if(teade.equals("newRoom")) {
+                    newChatRoom("Testikas",kasutaja);
+                } else if(teade.equals("showRooms")) {
+                    showChatRooms();
+                } else if(teade.equals("join")) {
+
+                }
+
+                String message = "Message from " + kasutaja + ": " + teade;
+                sendMessage(message, kasutaja);
+                System.out.println(message);
+
+            }
             activeSocket.close();
-        }catch (Exception e) {
-            System.out.println("test");
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void showChatRooms() {
-        for (ChatRoom room:chatRooms) {
+    public static void showChatRooms() {
+        for (ChatRoom room : chatRooms) {
             System.out.println(room);
+        }
+    }
+
+    public static void sendMessage(String message, User client) {
+
+        for (User s : clients) {
+            if (s != client) {
+                s.sendMessage(message);
+            }
+        }
+    }
+
+    public static void newChatRoom(String name, User u) {
+        ChatRoom cr = new ChatRoom(name,u);
+        chatRooms.add(cr);
+    }
+
+    public static void joinChatRoom(String chatroomName,User u) {
+
+        for (ChatRoom cr:chatRooms) {
+            if(cr.toString().equals(chatroomName)) {
+                cr.join(u);
+                String teade = u+"connected";
+            }
         }
     }
 }
